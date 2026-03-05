8c88f8534d7b65bcc54ce286d193e5e8a0953626
retz8 <ekkicb71@gmail.com>
2026-03-05 20:17:57 +0900

    fix: update catalog URI to retz8 GitHub username

--- libs/spartan-a2ui-adapter/src/lib/catalog-id.ts
+++ libs/spartan-a2ui-adapter/src/lib/catalog-id.ts
@@ -1,2 +1,2 @@
 export const SPARTAN_CATALOG_ID =
-  'https://github.com/jiohin/spartan-a2ui-adapter/blob/main/catalogs/spartan/v0.8.0/catalog.json';
+  'https://github.com/retz8/spartan-a2ui-adapter/blob/main/catalogs/spartan/v0.8.0/catalog.json';
