e4159603729a3b4ac835f2641965f905f189e35e
retz8 <ekkicb71@gmail.com>
2026-03-07 13:02:46 +0900

    feat: export HlmBadgeWrapperComponent from library barrel

--- libs/spartan-a2ui-adapter/src/index.ts
+++ libs/spartan-a2ui-adapter/src/index.ts
@@ -1,3 +1,4 @@
 export { SPARTAN_CATALOG } from './lib/catalog';
 export { SPARTAN_CATALOG_ID } from './lib/catalog-id';
 export { HlmButtonWrapperComponent } from './lib/components/hlm-button/hlm-button-wrapper.component';
+export { HlmBadgeWrapperComponent } from './lib/components/hlm-badge/hlm-badge-wrapper.component';
