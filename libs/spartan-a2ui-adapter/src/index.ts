9d92e5f9d1c070ba6cf647d83c062977e0727ba5
retz8 <ekkicb71@gmail.com>
2026-03-13 17:15:49 +0900

    chore: re install card

--- libs/spartan-a2ui-adapter/src/index.ts
+++ libs/spartan-a2ui-adapter/src/index.ts
@@ -2,3 +2,4 @@ export { SPARTAN_CATALOG } from './lib/catalog';
 export { SPARTAN_CATALOG_ID } from './lib/catalog-id';
 export { HlmButtonWrapperComponent } from './lib/components/hlm-button/hlm-button-wrapper.component';
 export { HlmBadgeWrapperComponent } from './lib/components/hlm-badge/hlm-badge-wrapper.component';
+export { HlmCardWrapperComponent } from './lib/components/hlm-card/hlm-card-wrapper.component'
